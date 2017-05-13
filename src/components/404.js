3c64ed6580bcdea730c4f3eaba579fa3c754dc35
monkingxue <1360662499@qq.com>
2017-05-13 20:53:56 +0800

    [feat] add demo file

--- src/components/404.js
+++ src/components/404.js
@@ -1,5 +1,9 @@
+// @flow
 import React, {Component} from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import "./404.scss";
+import bilibili from "./404.png";
 
 export default class NotFound extends Component {
   static contextTypes = {
@@ -9,7 +13,10 @@ export default class NotFound extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>404 NOT FOUND</div>
+        <div>
+          <div className="not-found">404 NOT FOUND</div>
+          <img src={bilibili}/>
+        </div>
       </MuiThemeProvider>
     );
   }
